5f72b8a8f139dafe6bbc7995f0a9711a5519d53b
Wen Ting Wang <42817036+chubberlisk@users.noreply.github.com>
2023-04-12 17:08:20 +0100

    Add API keys (#2)

--- api_gateway.tf
+++ api_gateway.tf
@@ -38,7 +38,7 @@ resource "aws_api_gateway_integration" "proxy_http_proxy" {
   }
 }
 
-resource "aws_api_gateway_deployment" "deployment" {
+resource "aws_api_gateway_deployment" "main" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
 
   # Force recreate of the deployment resource
@@ -53,3 +53,38 @@ resource "aws_api_gateway_deployment" "deployment" {
     create_before_destroy = true
   }
 }
+
+resource "aws_api_gateway_stage" "main" {
+  deployment_id = aws_api_gateway_deployment.main.id
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+  stage_name    = var.environment
+}
+
+resource "random_id" "api_keys" {
+  count       = length(var.clients)
+  byte_length = 16
+}
+
+resource "aws_api_gateway_api_key" "api_keys" {
+  count = length(var.clients)
+  name  = "${var.namespace}-${var.clients[count.index]}-key"
+  value = "${var.namespace}-${var.clients[count.index]}-${random_id.api_keys[count.index].hex}"
+}
+
+resource "aws_api_gateway_usage_plan" "main" {
+  name = "${var.namespace}-usage-plan"
+
+  api_stages {
+    api_id = aws_api_gateway_rest_api.api_gateway.id
+    stage  = aws_api_gateway_stage.main.stage_name
+  }
+}
+
+resource "aws_api_gateway_usage_plan_key" "main" {
+  count = length(var.clients)
+
+  key_id        = aws_api_gateway_api_key.api_keys[count.index].id
+  key_type      = "API_KEY"
+  usage_plan_id = aws_api_gateway_usage_plan.main.id
+}
+
